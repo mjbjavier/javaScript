10391124701c6d054b7f9d93e73ce8578562a75c
DevSingMB <mariobucheli@gmail.com>
2022-09-07 19:42:31 -0500

    valoreYvariablesJS

---
+++ valores.js
@@ -0,0 +1,22 @@
+// tipos de datos
+//Valores primitivos
+40 //número
+
+"Mario BUchely" //texto
+
+true
+false //para validar si tomamos una opción o no
+
+null
+undefined // valores reservados (placeholder) cuando nos falto un valor podriamos usarlo
+
+
+//valores no primitivos o tipo objeto
+//Array
+[1,2,3] 
+
+//valor tipo objeto
+{ nombre: "Mario"}
+
+
+
---
+++ variables.js
@@ -0,0 +1,20 @@
+var nombre = "Mario"; //sintaxys correcta de una variable 
+
+//dos estados de una variable
+//declarar 1er estado
+var edad;
+
+edad
+//inicializar 2do estado
+edad = 40;
+
+edad
+
+var elementos = ["Computador", "celular"]; //aquí estamos declarando e inicializando una variable
+
+var persona = {
+    nombre: "Mario",
+    edad: 30
+}
+
+persona;
