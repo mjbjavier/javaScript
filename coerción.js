b3e03ac3ebbc9d30493495f3b515f653fc1d12d8
DevSingMB <mariobucheli@gmail.com>
2022-09-08 18:06:26 -0500

    coerciónFuncionesEnJS

---
+++ coerción.js
@@ -0,0 +1,23 @@
+//undefined
+
+var b = a + "";
+//undefined
+console.log(b);
+//VM1648:1 20
+//undefined
+
+typeof b
+'string'
+var c = String(a);
+//undefined
+
+typeof c
+'string'
+console.log(c);
+//VM1790:1 20
+//undefined
+
+var d = Number(c);
+//undefined
+typeof d
+'number'
---
+++ funciones.js
@@ -0,0 +1,17 @@
+//conjunto de sentencias - cojunto de tareas - para hacer calculos - para validar algo
+//dos tipos de funciones 
+
+//declarativas fx que es declaritiva
+
+function funcion1() {
+    return 3;
+}
+funcion1(); // asi llamamos una fx
+
+//expresión fx que es expresiva
+
+var funcion1 = function(a,b ){
+    return a+b;
+}
+
+funcion1(); // asi llamamos una fx
