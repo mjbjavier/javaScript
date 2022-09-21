64879af1a0e8ae0e0a14b0c3e0a82f19b13e89cb
DevSingMB <mariobucheli@gmail.com>
2022-09-20 20:22:18 -0500

    obtenerResultadosDeUnaFx

--- fx.js
+++ fx.js
@@ -1,3 +1,31 @@
+//probar en estas expresiones en la consola del navegador
+
 function saludarPersonas(personas)  {
-    console.log(personas);
+    console.log(personas); //fx del navegador para imprimir en consola
 }
+
+saludarPersonas("Mario"); //Así llamamos la fx
+
+//alt gr + } = `` comillas invertidas en laptop
+//Template String o plantilla de cadena de texto de este modo puedo poner variables en un string
+function saludarPersonas(personas)  {
+    console.log(`Hola ${personas}`);//De esta forma lo conecto a la base de datos para que el saludo se haga de forma automatica
+}
+
+
+//¿como puedo sin console.log obtener resultados de un fx?
+//opción1
+function sumar(a,b) {
+    var resultado = a + b;
+    return resultado  // return es la palabra reservada para retornar algún resultado
+}
+
+//llamo la fx
+sumar(5,2);
+
+//opción2
+function sumar(a,b) {
+    return a + b;
+}
+
+sumar(5,7);
