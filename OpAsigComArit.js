ca333729a1ca5a9edf27e6f5179401490aa95b22
DevSingMB <mariobucheli@gmail.com>
2022-10-06 07:21:46 -0500

    InicioPlatziBecaOperadores

---
+++ OpAsigComArit.js
@@ -0,0 +1,42 @@
+//Operadores
+3 + 2
+//5
+50 - 10
+//40
+20 / 2
+//10
+
+"Mario" + "Buchely"
+
+//'MarioBuchely'
+"Mario " + "Buchely"
+
+//'Mario Buchely'
+// eñl caracter de ! es una negación
+
+!true
+//False
+
+//operadores de asignación =
+var a = 1; // = la inicializamos asignadole un valor
+
+3 == "3";
+//true
+
+3 === "3"; //en este caso el operardor valida que los dos operadores sean estrictamente iguales
+//false
+
+//otros tipos de comparación
+
+5 < 3
+//false
+
+5 > 3
+//true
+
+//validación de dos pasos
+5 <= 6
+// true
+
+
+
